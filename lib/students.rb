2bdac8227af8cb2ceb0df4c5fa4b3bed3499047e
Justin Winger <justinwinger4@gmail.com>
2020-04-16 20:13:43 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,10 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(GPA) FROM students.grade(9)"
 end
